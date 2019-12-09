2516e599b9bb7e33be09907a1fc5ccb290842fba
Chris Wright <chris@dxw.com>
2019-12-09 20:45:48 +0000

    Day 1 Part 1

---
+++ bash/01/fuel-requirements-p1.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+usage() {
+  echo "Usage: $(basename "$0") [OPTIONS]" 1>&2
+  echo "  -h               - help"
+  echo "  -i <input-file>  - Input file ( default '../../01/input.txt' )"
+  exit 1
+}
+
+INPUT_FILE="../../inputs/01/input.txt"
+
+while getopts "i:h" opt; do
+  case $opt in
+    i)
+      INPUT_FILE=$OPTARG
+      ;;
+    h)
+      usage
+      exit;;
+    *)
+      usage
+      exit;;
+  esac
+done
+
+# Create array from input file
+set +e
+IFS=$'\n' read -d '' -r -a MODULE_MASS < "$INPUT_FILE"
+set -e
+
+# Function to calculate required fuel
+# usage: required_fuel <module-mass>
+function required_fuel() {
+  local MASS
+  local REQUIRED_FUEL
+  MASS="$1"
+  REQUIRED_FUEL=$(($((MASS / 3)) - 2))
+  echo "$REQUIRED_FUEL"
+}
+
+# Calculate total required fuel
+TOTAL_FUEL=0
+for mass in "${MODULE_MASS[@]}"
+do
+  TOTAL_FUEL=$((TOTAL_FUEL + $(required_fuel "$mass")))
+done
+
+# Return total fuel required
+echo "$TOTAL_FUEL"
